b8971194e85509842e91f343b34c988e30ed0703
Ketan-Chaudhary <ketanchaudharypoonia@gmail.com>
2025-07-01 22:56:29 +0530

    Update Seo : TEST

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import ClientPage from "./ClientPage";
 
-export const metadata = {
-  title: "TC Lottery - Best Color Prediction Game | Win Unlimited Money",
-  description:
-    "Welcome to the TC Lottery official website. Play the best Color Prediction game with us and win unlimited money. 24×7 Support and fast withdrawals are our keys to your trust. Login or Register now and get sign-up bonus.",
-  keywords:
-    "tc lottery,tc lottery login link, tc win login, tc lottery vip prediction,color prediction game,vcw7t38771,referral code,sign up bonus,fast withdrawal,24x7 support,win money online,tclottery,account,active,amazing,android,anytime,best,blog,bonus,bonuses,card,casino,celebrate,choose,claim,code,community,conditions,connect,contact,copy,coupons,customer,days,deposit,deposits,download,earn,exciting,exclusive,experience,expires,friend,friends,games,gaming,gift,helped,home,instant,instantly,john,join,lottery,offer,opportunities,payouts,platform,play,players,policy,premier,privacy,referral,register,registration,reload,reward,rewards,secure,service,share,sign,simple,special,start,strategies,style,support,telegram,terms,thousands,time,today,toggle,unlock,updates,using,variety,weekend,welcome,winner,winners,winning,winnings,wins,works,colour games,daily gift codes,mobile lottery app,lottery bonuses,referral bonuses,color trading,color prediction,colour prediction,colour chart,colour trading tips,trade colours,colour profit,betting on colours,red green blue trading,colour win,colour signal,real-time colour trading,colour forecast,live colour trading,predict colours,colour market,colour game tips,colour patterns,colour betting strategy,colour trends",
-};
+// export const metadata = {
+//   title: "TC Lottery - Best Color Prediction Game | Win Unlimited Money",
+//   description:
+//     "Welcome to the TC Lottery official website. Play the best Color Prediction game with us and win unlimited money. 24×7 Support and fast withdrawals are our keys to your trust. Login or Register now and get sign-up bonus.",
+//   keywords:
+//     "tc lottery,tc lottery login link, tc win login, tc lottery vip prediction,color prediction game,vcw7t38771,referral code,sign up bonus,fast withdrawal,24x7 support,win money online,tclottery,account,active,amazing,android,anytime,best,blog,bonus,bonuses,card,casino,celebrate,choose,claim,code,community,conditions,connect,contact,copy,coupons,customer,days,deposit,deposits,download,earn,exciting,exclusive,experience,expires,friend,friends,games,gaming,gift,helped,home,instant,instantly,john,join,lottery,offer,opportunities,payouts,platform,play,players,policy,premier,privacy,referral,register,registration,reload,reward,rewards,secure,service,share,sign,simple,special,start,strategies,style,support,telegram,terms,thousands,time,today,toggle,unlock,updates,using,variety,weekend,welcome,winner,winners,winning,winnings,wins,works,colour games,daily gift codes,mobile lottery app,lottery bonuses,referral bonuses,color trading,color prediction,colour prediction,colour chart,colour trading tips,trade colours,colour profit,betting on colours,red green blue trading,colour win,colour signal,real-time colour trading,colour forecast,live colour trading,predict colours,colour market,colour game tips,colour patterns,colour betting strategy,colour trends",
+// };
 
 export default function Home() {
   return <ClientPage />;
